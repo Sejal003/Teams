c06524b988bd07bb7557711bd807b967b915e232
Sejal003 <123571005+Sejal003@users.noreply.github.com>
2024-03-19 10:38:10 -0700

    Teams page

---
+++ src/components/Team.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import '../styles/team.css'
+import team01 from '../images/picture.png'
+
+const teamMembers = [
+    {
+        imgUrl: team01,
+        name: 'Name',
+        position: 'Production Designer'
+    },
+
+    {
+        imgUrl: team01,
+        name: 'Name',
+        position: 'Product Designer'
+    },
+
+    {
+        imgUrl: team01,
+        name: 'Name',
+        position: 'Customer Service'
+    },
+
+    {
+        imgUrl: team01,
+        name: 'Name',
+        position: 'Project Leader'
+    },
+]
+const teamMembers2 = [
+    {
+        imgUrl: team01,
+        name: 'Name',
+        position: 'Production Designer'
+    },
+
+    {
+        imgUrl: team01,
+        name: 'Name',
+        position: 'Product Designer'
+    },
+
+    {
+        imgUrl: team01,
+        name: 'Name',
+        position: 'Customer Service'
+    },
+]
+
+const Team = () => {
+    return (
+        <section className='our__team'>
+            <div className='container'>
+                <div className='team__content'>
+                    <h6 className='subtitle'>Meet Our Team</h6>
+                    <h2>
+                        Leadership/Management Team
+                    </h2>
+                </div>
+                <div className='team__wrapper'>
+                    {
+                        teamMembers.map((item, index) => (
+                            <div className='team__item' key={index}>
+                                <div className='team__img'>
+                                    <img src={item.imgUrl} alt='' />
+                                </div>
+                                <div className='team__details'>
+                                    <h4>{item.name}</h4>
+                                    <p className='description'>{item.position}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className='team__wrapper'>
+                    {
+                        teamMembers2.map((item, index) => (
+                            <div className='team__item' key={index}>
+                                <div className='team__img'>
+                                    <img src={item.imgUrl} alt='' />
+                                </div>
+                                <div className='team__details'>
+                                    <h4>{item.name}</h4>
+                                    <p className='description'>{item.position}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Team
